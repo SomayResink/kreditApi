28b894641537653a45a484e8c9b2b52d9560153d
rhann7 <raihandityam09@gmail.com>
2025-09-04 12:01:56 +0700

    fix RoleMiddleware & tokens table

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -2,23 +2,16 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Support\Facades\Auth;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     */
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (!Auth::check()) {
-            return response()->json(['message' => 'Unauthorized'], 401);
-        }
-
         if (!in_array(Auth::user()->role, $roles)) {
-            return response()->json(['message' => 'Forbidden'], 403);
+            return response()->json(['error' => 'Unauthorized'], 403);
         }
 
         return $next($request);
